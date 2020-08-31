cb0774a499bb18087136a855b73902c3ad0c42c0
bshabash <bshabash007@outlook.com>
2020-08-30 19:09:51 -0700

    Simplified the problem into 3 classes for mel spectrograms

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 A capstone project demo for prediction of heart abnormalities
 
 Data obtained from: https://www.kaggle.com/kinguistics/heartbeat-sounds?select=set_a
+
+## Requirements
+
+This will follow more detailed instructions but the major requirements are
+`scikit-learn 0.23.2`
+`numba 0.48`
+`librosa 0.6.3`
--- boris_util.py
+++ boris_util.py
@@ -103,5 +103,69 @@ def read_original_data(folder_location):
 
 
 
+############################################################
+def read_and_combine_data(folder_location, read_noisy_data=True):
+
+    '''
+        This function reads the files present in 
+        [folder_location] and returns the files
+        as a single dataframe containing
+        the file names
+        and the label as the target.
+
+    '''
+
+    # Read files and drop unlabeled test set used on Kaggle
+    set_a_df = pd.read_csv(folder_location+'/set_a.csv')
+    set_b_df = pd.read_csv(folder_location+'/set_b.csv')
+
+    # remove unlabeled files within each dataframe, all other files are named in the CSV
+    # as they are on the hard drive
+    set_a_df = set_a_df[~set_a_df['label'].isna()]
+    set_b_df = set_b_df[~set_b_df['label'].isna()]
+
+    # Perform string operations to match names to their filename on the hard drive
+
+    set_b_df['fname'] = set_b_df['fname'].str.replace('Btraining_', '')
+    set_b_df['fname'] = set_b_df['fname'].str.replace('normal_', 'normal__')
+    set_b_df['fname'] = set_b_df['fname'].str.replace('murmur_', 'murmur__')
+    set_b_df['fname'] = set_b_df['fname'].str.replace('extrastole_', 'extrastole__')
+    set_b_df['fname'] = set_b_df['fname'].str.replace('normal__noisynormal__', 'normal_noisynormal_')
+    set_b_df['fname'] = set_b_df['fname'].str.replace('murmur__noisymurmur__', 'murmur_noisymurmur_')
+
+    # Combine the dataframes
+    combined_df = pd.concat([set_a_df, set_b_df], axis=0)
+
+    # Reset the index and then drop the old index
+    combined_df = combined_df.reset_index(drop=True).drop(['sublabel', 'dataset'], axis=1)
+
+    if not (read_noisy_data):
+        combined_df = combined_df[~combined_df['fname'].str.contains('noisy')]
+
+    # read the signals and sampling rates into two lists
+    signals = []
+    sampling_rates = []
+
+    
+
+    for index, row in combined_df.iterrows():
+        y, sr = librosa.load(folder_location+"/"+row['fname'])
+        signals.append(y)
+        sampling_rates.append(sr)
+
+        print(f"processed {round(100*index/combined_df.shape[0], 2)}%", end="\r")
+
+    # then conver them into a dataframe
+    signal_df = pd.DataFrame({'signal': signals, 
+                              'sampling_rate': sampling_rates,
+                              'label': combined_df['label']})
+
+
+    return signal_df
+############################################################
+############################################################
+############################################################
+############################################################
+############################################################
 
 
